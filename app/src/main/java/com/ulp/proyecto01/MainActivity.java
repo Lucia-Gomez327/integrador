5df8f0440388d9779a4c5557a4922abb76bcd8b2
Lucia-Gomez327 <luciagomez964@yahoo.com>
2019-08-21 17:24:21 -0300

    Initial commit

---
+++ app/src/main/java/com/ulp/proyecto01/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ulp.proyecto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BroadcastReceiver br = new MyBrodcastReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(br, filter);
+        Intent q= getIntent();
+         q.setAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+
+
+        br.onReceive(null,q);
+    }
+
+
+}
